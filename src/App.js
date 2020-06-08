46ec4bbf79776226cda676a7531b2e38ea83dc06
MAEDA Yasuhiko <y.maeda@dongoon.jp>
2020-06-08 10:27:06 +0900

    subscribe list query

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { API, graphqlOperation } from "aws-amplify";
+import { listPosts } from './graphql/queries';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    posts: []
+  }
+
+  async componentDidMount() {
+    try {
+      const posts = await API.graphql(graphqlOperation(listPosts))
+      console.log('posts: ', posts)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    )
+  }
 }
 
 export default App;
